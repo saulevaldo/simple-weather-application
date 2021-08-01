9d474cd795a6768aaf9c7566c830d831012ef2e1
Saule <soolas@yahoo.com>
2021-08-01 15:10:59 +0200

    removed fahrenheit coversion

--- src/app.js
+++ src/app.js
@@ -77,7 +77,7 @@ function getForecast(coordinates) {
 
 function displayTemperature(response) {
   let showTemperature = document.querySelector("#temperature");
-  showTemperature.innerHTML = Math.round(celsius);
+  showTemperature.innerHTML = Math.round(response.data.main.temp);
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
   let descriptionOfCondition = document.querySelector("#description");
@@ -86,8 +86,6 @@ function displayTemperature(response) {
   showHumidity.innerHTML = response.data.main.humidity;
   let showWind = document.querySelector("#wind");
   showWind.innerHTML = Math.round(response.data.wind.speed);
-
-  celsius = response.data.main.temp;
 
   let date = document.querySelector("#date");
   date.innerHTML = displayDate(response.data.dt * 1000);
@@ -115,30 +113,5 @@ function handleSubmit(event) {
 
 let searchForm = document.querySelector("#search");
 searchForm.addEventListener("submit", handleSubmit);
-
-function changeToFahrenheit(event) {
-  event.preventDefault();
-  showCelsius.classList.remove("active");
-  showFahrenheit.classList.add("active");
-  let fahrenheitTemp = (celsius * 9) / 5 + 32;
-  let temperature = document.querySelector("#temperature");
-  temperature.innerHTML = Math.round(fahrenheitTemp);
-}
-
-function changeToCelsius(event) {
-  event.preventDefault();
-  showCelsius.classList.add("active");
-  showFahrenheit.classList.remove("active");
-  let temperature = document.querySelector("#temperature");
-  temperature.innerHTML = Math.round(celsius);
-}
-
-let celsius = null;
-
-let showFahrenheit = document.querySelector("#fahrenheit");
-showFahrenheit.addEventListener("click", changeToFahrenheit);
-
-let showCelsius = document.querySelector("#celsius");
-showCelsius.addEventListener("click", changeToCelsius);
 
 search("Oslo");
